44c7d35adfb2fdfe3f49221bcc217f66cc443ea6
roostercrab <jesse.shea@qixingdesign.com>
2020-09-20 10:08:35 -0700

    collision detected

--- game.py
+++ game.py
@@ -29,24 +29,32 @@ enemy_y_coord = enemy_pos[1]
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # FUNCTIONS
-def detect_collision(p_x, p_y, e_x, e_y):
-  if (e_x >= p_x
-      and
-      e_x < (p_x + player_size)) or (p_x >= e_x
-                                    and
-                                    p_x < (e_x + enemy_size)):
-        if (e_y >= p_y
-            and
-            e_y < (p_y + player_size)
-            or
-            (p_y >= e_y
-            and
-            p_y < (e_y + enemy_size)
-            )
-            ):
-              return True
-        return False
+def detect_collision(player_x_coord, player_y_coord, enemy_x_coord, enemy_y_coord):
+  player_x_coord = int(player_x_coord)
+  player_y_coord = int(player_y_coord)
+  enemy_x_coord = int(enemy_x_coord)
+  enemy_y_coord = int(enemy_y_coord)
 
+  player_x_space = range(player_x_coord, player_x_coord + player_size)
+  player_y_space = range(player_y_coord, player_y_coord + player_size)
+
+  enemy_x_space = range(enemy_x_coord, enemy_x_coord + enemy_size)
+  enemy_y_space = range(enemy_y_coord, enemy_y_coord + enemy_size)
+
+  player_x_set = set(player_x_space)
+  enemy_x_set = set(enemy_x_space)
+  x_set_result = player_x_set.intersection(enemy_x_set)
+
+  player_y_set = set(player_y_space)
+  enemy_y_set = set(enemy_y_space)
+  y_set_result = player_y_set.intersection(enemy_y_set)
+
+  if x_set_result and y_set_result != set():
+    print("GAME OVER")
+    return True
+  else:
+    print("game not over")
+    return False
 
 # GAME LOOP
 game_over = False
@@ -67,6 +75,8 @@ while not game_over:
 
       player_pos[0] = player_x_coord
 
+  # PAINT BACKGROUND
+  screen.fill(BACKGROUND_COLOR)
 
   # ENEMY BLOCK MOVEMENT
   if enemy_y_coord >= 0 and enemy_y_coord < SCREEN_HEIGHT:
@@ -75,8 +85,8 @@ while not game_over:
     enemy_x_coord = random.randint(0, SCREEN_WIDTH - enemy_size)
     enemy_y_coord = 0
 
-  # PAINT BACKGROUND
-  screen.fill(BACKGROUND_COLOR)
+  if detect_collision(player_x_coord, player_y_coord, enemy_x_coord, enemy_y_coord):
+    game_over = True
 
   # PLAYER IMAGE DRAW
   pygame.draw.rect(screen, RED, (player_x_coord, player_y_coord, player_size, player_size))
